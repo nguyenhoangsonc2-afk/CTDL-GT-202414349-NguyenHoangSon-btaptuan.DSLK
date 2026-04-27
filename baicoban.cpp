a4c5d762ff7318cff2f515b5d3364ec70adeae3c
nguyenhoangsonc2-afk <nguyenhoangsonc2@gmail.com>
2026-04-27 17:52:27 +0700

    version1

---
+++ baicoban.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+// khai báo ctdl
+
+struct Ngay {
+    int ngay, thang, nam;
+};
+
+struct SinhVien {
+    char maSV[8];     
+    char hoTen[50];
+    int gioiTinh;
+    Ngay ngaySinh;
+    char diaChi[100];
+    char lop[12];
+    char khoa[7];      
+};
+
+struct Node {
+    SinhVien data;
+    Node *link;
+};
+
+struct List {
+    Node *first;
+    Node *last;
+};
+
+// các hàm
+
+void khoiTao(List &ds) {
+    ds.first = ds.last = NULL;
+}
+
+Node* taoNode(SinhVien sv) {
+    Node* p = new Node;
+    p->data = sv;
+    p->link = NULL;
+    return p;
+}
+//Thêm có thứ tự
+void themCoThuTu(List &ds, SinhVien sv) {
+    Node* p = taoNode(sv);
+
+    if (ds.first == NULL || strcmp(sv.maSV, ds.first->data.maSV) < 0) {
+        p->link = ds.first;
+        ds.first = p;
+        if (ds.last == NULL) ds.last = p;
+    } else {
+        Node* hienTai = ds.first;
+        while (hienTai->link != NULL &&
+               strcmp(hienTai->link->data.maSV, sv.maSV) < 0) {
+            hienTai = hienTai->link;
+        }
+        p->link = hienTai->link;
+        hienTai->link = p;
+        if (p->link == NULL) ds.last = p;
+    }
+}
+
+// Xuất
+void xuatSinhVien(SinhVien sv) {
+    cout << "[" << sv.maSV << "] " << sv.hoTen << " | "
+         << sv.ngaySinh.ngay << "/" << sv.ngaySinh.thang << "/" << sv.ngaySinh.nam
+         << " | " << sv.lop << " | " << sv.khoa << endl;
+}
+
+void xuatDanhSach(List ds) {
+    if (ds.first == NULL) {
+        cout << "(Danh sach rong)" << endl;
+        return;
+    }
+    Node* p = ds.first;
+    while (p != NULL) {
+        xuatSinhVien(p->data);
+        p = p->link;
+    }
+}
+
+
+int main() {
+     List dsSV;
+    khoiTao(dsSV);
+
+    //CASE 1
+    cout << "CASE 1: DANH SACH TRONG\n";
+    xuatDanhSach(dsSV);
+    xuatDanhSach(dsSV);
+     return 0;
+}
